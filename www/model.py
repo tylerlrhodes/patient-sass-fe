8d24f9e699a4330564417737019f03af863a45e2
Victor <victorcortezcb@gmail.com>
2020-04-11 21:59:49 +0000

    Updated the model to include more entities of joanne's design. Soon I will add the rest.

--- www/model.py
+++ www/model.py
@@ -2,19 +2,50 @@ import flask_sqlalchemy
 
 db = flask_sqlalchemy.SQLAlchemy()
 
-class User(db.Model):
-    __tablename__ = "user"
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), index=True, unique=True)
-    email = db.Column(db.String(120), index=True, unique=True)
-    password_hash = db.Column(db.String(128))
 
+class User(db.Model):
+    id = db.Column(db.BigInteger, primary_key=True)
+    name = db.Column(db.String(128), index=True, unique=True)
+    email = db.Column(db.String(128), index=True, unique=True)
+    role = db.Column(db.String(128))
+    password_hash = db.Column(db.String(128))
+    tenant_id = db.relationship('Tenant', backref="user", lazy="dynamic")
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
-class Patient(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), unique=False, nullable=False)
+class Tenant(db.Model):
+    id = db.Column(db.BigInteger, primary_key=True)
+    name = db.Column(db.String(64), index=True)
+    url = db.Column(db.String(128), index=True, unique=True)
+    status = db.Column(db.String(64))
+    create_datetime = db.Column(db.DateTime)
+    def __repr__(self):
+        return '<Tenant {}>'.format(self.name)
 
+
+class PatientLogin(db.Model):
+    patient_id = db.relationship('Patient',backref="login", lazy="dynamic")
+    id = db.Column(db.BigInteger, primary_key=True)
+    password_hash = db.Column(db.String(128))
+    last_login_datetime = db.Column(db.DateTime)
+    def __repr__(self):
+            return 'Patient id: %r | Time: %r' % (self.patient_id,self.last_login_datetime)
+class Patient(db.Model):
+    id = db.Column(db.BigInteger, primary_key=True)
+    name = db.Column(db.String(128), unique=False, nullable=False)
+    email = db.Column(db.String(128), index=True, unique=True)
+    mobile_number = db.Column(db.string(25))
+    postal_address = db.Column(db.Integer)
+    create_datetime = db.Column(db.DateTime)
+    tenant_id = db.relationship('Tenant', backref="patient", lazy="dynamic")
     def __repr__(self):
         return 'Patient: %r' % self.name
+
+class PatientRequest(db.Model):
+    id = db.Column(db.BigInteger, primary_key=True)
+    patient_id = db.relationship('Patient', backref="request", lazy="dynamic")
+    category_id = db.Column(db.BigInteger)
+    status = db.Column(db.String(128), unique=False, nullable=False)
+    create_datetime = db.Column(db.DateTime)
+    last_update_datetime = db.Column(db.DateTime)
+    last_updated_user_id = db.relationship('User', backref="request", lazy="dynamic")
